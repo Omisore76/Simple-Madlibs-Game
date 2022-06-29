961215fd404030f8b939aade0d2edd52111142c4
Omisore Dotun <78863472+Omisore76@users.noreply.github.com>
2022-06-29 20:17:54 +0100

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,17 @@
+# Simple madlibs game.
+
+# This game prompts a user for a list of words to substitute for blanks in a story.
+
+Programming_Language = input("Enter a programming language of your choice: ")
+Noun = input ("Enter a noun: ")
+Noun2 = input ("Enter another noun: ")
+madlibs = f"{Programming_Language} is a friendly programming language that makes coding easy and fun.\
+\nIt's perfect for {Noun} who want to learn how to code, and experienced {Noun2} who want to work\
+more efficiently. \n{Programming_Language} is used by top companies like Google, Facebook, Netflix, and more.\
+ So why not give it a try?"
+
+print(madlibs)
+
+# I chose Python as my Programming_Language. Feel free to play around with it.
+# I chose beginners as my noun.
+# "Developers" is my second noun.
